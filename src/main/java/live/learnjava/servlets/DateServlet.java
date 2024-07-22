082925e999200358a9109cfc130a90d6d8b12e25
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-07-22 17:40:31 -0400

    completed app

---
+++ src/main/java/live/learnjava/servlets/DateServlet.java
@@ -0,0 +1,41 @@
+package live.learnjava.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/dateurl", name="DateServlet")
+public class DateServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Date Servlet Works!");
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		Date d = new Date();
+		pw.println("Date::" + d);
+		pw.println("<br>");
+		pw.println("<a href='index.jsp'>home</a>");
+		pw.close();		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	
+	
+}
